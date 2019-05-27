a93184cba15a2f9388ba4116fba73496a146a498
Nguyễn Cao Trường Phong <phong.busbby@gmail.com>
2019-05-28 01:46:30 +0700

    function arrays and coding challenge 3

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -270,6 +270,8 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 
 GOOD LUCK 😀
 */
+
+/*
 var johnTeamGame1Score = 118
 var johnTeamGame2Score = 97
 var johnTeamGame3Score = 127
@@ -300,3 +302,142 @@ if (johnTeamAverageScore > mikeTeamAverageScore && johnTeamAverageScore > maryTe
 }   else {
         console.log('There is a draw!')
 }
+
+*/
+
+/*****************************
+* FUNCTIONS
+*/
+
+/*
+function calculateAge(birthYear) {
+    return 2018 - birthYear
+}
+
+var ageJohn = calculateAge(1994)
+var ageMike = calculateAge(1996)
+var ageJane = calculateAge(1998)
+
+console.log(ageJohn, ageMike, ageJane)
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year)
+    var retirement = 60 - age
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.')
+    } else {
+        console.log(firstName + ' is already retired.')
+    }
+}
+
+yearsUntilRetirement(1990, 'John')
+yearsUntilRetirement(1948, 'Mike')
+
+*/
+
+/*****************************
+* Function Statements and Expressions
+*/
+
+/*
+// Function declaration
+// function whatDoYouDo(job, firstName) {}
+
+// Function Expression
+var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code'
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.'
+        case 'designer':
+            return firstName + ' designs beautiful websites.'
+        default:
+            return firstName + ' does something else.'
+    }
+}
+
+console.log(whatDoYouDo('teacher', 'John'))
+console.log(whatDoYouDo('designer', 'Jane'))
+*/
+
+/*****************************
+* Arrays
+*/
+
+/*
+// Initialize new array
+var names = ['John', 'Mark', 'Jane']
+var years = new Array(1990, 1969, 1948)
+
+console.log(names[0])
+console.log(names.length)
+
+// Mutate array data
+names[1] = 'Ben'
+names[names.length] = 'Mary'
+console.log(names)
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer',  false]
+
+john.push('blue')
+john.unshift('Mr.')
+console.log(john)
+
+console.log(john.pop())
+john.shift()
+console.log(john)
+
+console.log(john.indexOf(23))
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : "John IS a designer"
+console.log(isDesigner)
+
+*/
+
+/*****************************
+* Coding Challenge 3
+*/
+
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+
+var bill1 = 124
+var bill2 = 48
+var bill3 = 268
+
+var tipCaculator = function (bill) {
+    var percentage
+    if (bill < 50) {
+        percentage = 0.2
+    } else if (bill <= 200) {
+        percentage =  0.15
+    } else {
+        percentage = 0.1
+    }
+    return bill * percentage
+}
+
+var allTips = []
+allTips.push(tipCaculator(bill1))
+allTips.push(tipCaculator(bill2))
+allTips.push(tipCaculator(bill3))
+console.log(allTips)
+
+var allPaid = []
+allPaid.push(tipCaculator(bill1) + bill1)
+allPaid.push(tipCaculator(bill2) + bill2)
+allPaid.push(tipCaculator(bill3) + bill3)
+console.log(allPaid)
