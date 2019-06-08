bc630c9b9c1415c24c945dec0ac266f707527bfc
Nguyen Cao Truong Phong <phongctnguyen@gmail.com>
2019-06-09 03:09:11 +0700

    first class function and closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -54,6 +54,8 @@ var jane = Object.create(personProto, {
 })
 */
 
+/* 
+
 // Primitives vs objects
 var a = 23
 var b = a
@@ -87,3 +89,127 @@ function change (a, b) {
 change(age, obj)
 console.log(age)
 console.log(obj.city)
+
+*/
+
+/*
+// Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998]
+
+function arrayCalc(arr, fn) {
+	var arrayRes = []
+	for (var i = 0; i < arr.length; i++) {
+		arrayRes.push(fn(arr[i]))
+	}
+	return arrayRes
+}
+
+function calculateAge(el) {
+	return 2019 - el
+}
+
+function isFullAge(el) {
+	return el >= 18
+}
+
+function maxHeartRate(el) {
+	if (el >= 18) {
+		return Math.round(206.9 - 0.67 * el)
+	} else {
+		return -1
+	}
+}
+
+var ages = arrayCalc(years, calculateAge)
+var fullAges = arrayCalc(ages, isFullAge)
+var rates = arrayCalc(ages, maxHeartRate)
+
+console.log(ages)
+console.log(fullAges)
+console.log(rates)
+
+*/
+
+/*
+// Functions returning functions
+function interviewQuestion(job) {
+	if (job === 'designer') {
+		return function(name) {
+			console.log(name + ', can you please explain what UX design is?')
+		}
+	} else if (job === 'teacher') {
+		return function(name) {
+			console.log('What subject do you teach, ' + name + '?')
+		}
+	} else {
+		return function(name) {
+			console.log('Hello ' + name + ', what do you do?')
+		}
+	}
+}
+
+var teacherQuestion = interviewQuestion('teacher')
+var designerQuestion = interviewQuestion('designer')
+
+teacherQuestion('John')
+designerQuestion('John')
+designerQuestion('Jane')
+
+interviewQuestion('teacher')('Mark')
+*/
+
+// Immediately invoked function expressions (IIFE)
+/*
+function game() {
+	var score = Math.random() * 10
+	console.log(score >= 5)
+}
+
+game()
+*/
+
+/*
+;(function() {
+	var score = Math.random() * 10
+	console.log(score >= 5)
+})()
+
+//console.log(score)
+;(function(goodLuck) {
+	var score = Math.random() * 10
+	console.log(score >= 5 - goodLuck)
+})(5)
+*/
+
+// Closure
+function retirement(retirementAge) {
+	var a = ' years until retirement.'
+	return function(yearOfBirth) {
+		var age = 2019 - yearOfBirth
+		console.log(retirementAge - age + a)
+	}
+}
+
+var retirementUS = retirement(66)
+var retirementGermany = retirement(65)
+var retirementVietNam = retirement(55)
+
+retirementUS(1990)
+retirementGermany(1992)
+retirementVietNam(1994)
+retirement(66)(1990)
+
+function interviewQuestion(job) {
+	return function(name) {
+		if (job === 'designer')
+			console.log(name + ', can you please explain what UX design is?')
+		else if (job === 'teacher')
+			console.log('What subject do you teach, ' + name + '?')
+		else console.log('Hello ' + name + ', what do you do?')
+	}
+}
+
+interviewQuestion('designer')('Wayne')
+interviewQuestion('teacher')('Daniel')
+interviewQuestion('it')('Santi')
