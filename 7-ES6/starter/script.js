1317be3bde5aefb3592e08147990ac52091ae449
Nguyễn Cao Trường Phong <phong.busbby@gmail.com>
2019-06-10 22:35:14 +0700

    string and arrow function es6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -14,6 +14,7 @@ name6 = 'Jane Miller'
 console.log(name6)
 */
 
+/*
 // ES5
 function driversLicense5(passedTest) {
     if (passedTest) {
@@ -46,3 +47,71 @@ for (let i = 0; i < 5; i++) {
 }
 
  console.log(i)
+ */
+
+ // Blocks and IIFEs
+ // ES6 
+ /*
+ {
+    const a = 1
+    let b = 2
+ }
+
+ console.log(c)
+
+ // ES5
+ (function() {
+    var c = 3
+ })()
+
+ // console.log(c)
+*/
+
+/*
+// Strings
+
+let firstName = 'John'
+let lastName = 'Smith'
+const yearOfBirth = 1990
+
+function calcAge(year) {
+    return 2019 - year
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today he is ' + calcAge(yearOfBirth) + ' years old.')
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`)
+
+const n = `${firstName} ${lastName}`
+console.log(n.startsWith('J'))
+console.log(n.endsWith('th'))
+console.log(n.includes('oh'))
+console.log(firstName.repeat(5))
+*/
+
+// Arrow Function
+
+const years = [1990, 1965, 1982, 1937]
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2019 - el
+})
+
+console.log(ages5)
+
+// ES6
+let ages6 = years.map(el => 2019 - el)
+console.log(ages6)
+
+ages6 = years.map((el, index) => `Age element ${index + 1} : ${2019 - el}.`)
+console.log(ages6)
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear()
+    const age = now - el
+    return `Age element ${index + 1} : ${age}.`
+})
+console.log(ages6)
