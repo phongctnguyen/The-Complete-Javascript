e09a16b2d0c5df8a44247445d6fdd9a033daabf6
Nguyen Cao Truong Phong <phongctnguyen@gmail.com>
2019-06-11 20:32:35 +0700

    finish arrow function

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -49,9 +49,9 @@ for (let i = 0; i < 5; i++) {
  console.log(i)
  */
 
- // Blocks and IIFEs
- // ES6 
- /*
+// Blocks and IIFEs
+// ES6
+/*
  {
     const a = 1
     let b = 2
@@ -91,6 +91,7 @@ console.log(n.includes('oh'))
 console.log(firstName.repeat(5))
 */
 
+/*
 // Arrow Function
 
 const years = [1990, 1965, 1982, 1937]
@@ -115,3 +116,94 @@ ages6 = years.map((el, index) => {
     return `Age element ${index + 1} : ${age}.`
 })
 console.log(ages6)
+*/
+
+// ES5
+
+var box5 = {
+	color: 'green',
+	position: 1,
+	clickMe: function() {
+		var self = this
+		document.querySelector('.green').addEventListener('click', function() {
+			var str =
+				'This is box number ' + self.position + ' and it is ' + self.color
+			alert(str)
+		})
+	}
+}
+
+// box5.clickMe()
+
+//ES6
+const box6 = {
+	color: 'green',
+	position: 1,
+	clickMe: function() {
+		document.querySelector('.green').addEventListener('click', () => {
+			var str =
+				'This is box number ' + this.position + ' and it is ' + this.color
+			alert(str)
+		})
+	}
+}
+
+// box6.clickMe()
+
+// const box66 = {
+// 	color: 'green',
+// 	position: 1,
+// 	clickMe: function() {
+// 		document.querySelector('.green').addEventListener('click', () => {
+// 			var str =
+// 				'This is box number ' + this.position + ' and it is ' + this.color
+// 			alert(str)
+// 		})
+// 	}
+// }
+
+// box66.clickMe()
+
+// const box66 = {
+// 	color: 'green',
+// 	position: 1,
+// 	clickMe: () => {
+// 		document.querySelector('.green').addEventListener('click', () => {
+// 			var str =
+// 				'This is box number ' + this.position + ' and it is ' + this.color
+// 			alert(str)
+// 		})
+// 	}
+// }
+
+// box66.clickMe()
+
+// ES5
+function Person(name) {
+	this.name = name
+}
+
+Person.prototype.myFriends5 = function(friends) {
+	var arr = friends.map(
+		function(el) {
+			return this.name + ' is friends with ' + el
+		}.bind(this)
+	)
+	console.log(arr)
+}
+
+var friends = ['Bob', 'Jane', 'Mark']
+new Person('John').myFriends5(friends)
+
+// ES6
+function Person(name) {
+	this.name = name
+}
+
+Person.prototype.myFriends6 = function(friends) {
+	var arr = friends.map(el => `${this.name} is friends with ${el}`)
+	console.log(arr)
+}
+
+var friends = ['Bob', 'Jane', 'Mark']
+new Person('Mike').myFriends6(friends)
