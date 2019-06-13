2942ed865f188840b64aefbd5f9fdcdd6e8c4253
Nguyen Cao Truong Phong <phongctnguyen@gmail.com>
2019-06-14 01:35:50 +0700

    function parameters & Map

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -300,6 +300,7 @@ console.log(ages.findIndex(cur => cur >= 18))
 console.log(ages.find(cur => cur >= 18))
 */
 
+/*
 // Spread operator
 function addFourAges(a, b, c, d) {
     return a + b + c + d
@@ -330,3 +331,120 @@ const boxes = document.querySelectorAll('.box')
 const all = [h, ...boxes]
 
 Array.from(all).forEach(cur => cur.style.color = 'purple')
+*/
+
+// Rest parameters
+
+/*
+// ES5
+function isFullAge5() {
+	//console.log(arguments)
+	var argsArr = Array.prototype.slice.call(arguments)
+
+	argsArr.forEach(function(cur) {
+		console.log(2019 - cur >= 18)
+	})
+}
+
+//isFullAge5(1990, 1999, 1965)
+//isFullAge5(1990, 1999, 1965, 2019, 1987)
+
+// ES6
+function isFullAge6(...years) {
+	years.forEach(cur => console.log(2016 - cur >= 18))
+}
+
+isFullAge6(1990, 1999, 1965, 2016, 1987)
+*/
+
+/*
+function isFullAge5(limit) {
+	//console.log(arguments)
+	var argsArr = Array.prototype.slice.call(arguments)
+
+	argsArr.forEach(function(cur) {
+		console.log(2019 - cur >= limit)
+	})
+}
+
+//isFullAge5(16, 1990, 1999, 1965)
+//isFullAge5(1990, 1999, 1965, 2019, 1987)
+
+// ES6
+function isFullAge6(limit, ...years) {
+	years.forEach(cur => console.log(2016 - cur >= limit))
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987)
+*/
+
+// Default parameters
+
+// ES5
+/*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+	lastName === undefined ? (lastName = 'Smith') : lastName
+	nationality === undefined ? (nationality = 'american') : nationality
+	this.firstName = firstName
+	this.lastName = lastName
+	this.yearOfBirth = yearOfBirth
+	this.nationality = nationality
+}
+
+// ES6
+
+function SmithPerson(
+	firstName,
+	yearOfBirth,
+	lastName = 'Smith',
+	nationality = 'american'
+) {
+	this.firstName = firstName
+	this.lastName = lastName
+	this.yearOfBirth = yearOfBirth
+	this.nationality = nationality
+}
+
+var john = new SmithPerson('John', 1990)
+
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
+*/
+
+// Maps
+
+const question = new Map()
+question.set(
+	'question',
+	'What is the official name of the lastest major Javascript version?'
+)
+question.set(1, 'ES5')
+question.set(2, 'ES6')
+question.set(3, 'ES2015')
+question.set(4, 'ES7')
+question.set('correct', 3)
+question.set(true, 'Correct answer :D')
+question.set(false, 'Wrong, please try again')
+
+console.log(question.get('question'))
+console.log(question.size)
+
+if (question.has(4)) {
+	//question.delete(4)
+	console.log('Answer 4 is here')
+}
+
+//question.clear()
+
+//question.forEach((value, key) => console.log(`This is ${key} and it's set to ${value}`))
+
+for (let [key, value] of question.entries()) {
+	if (typeof key === 'number') {
+		if (typeof key === 'number') {
+			console.log(`Answer ${key}: ${value}`)
+		}
+	}
+}
+
+const ans = parseInt(prompt('Write the corrent answer'))
+
+console.log(question.get(ans === question.get('correct')))
